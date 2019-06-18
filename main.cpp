7343d470a74fe5b8eb8960d6aea7d1faa2cfdaa5
Nikolay Nanev <niki4etooo@gmail.com>
2019-06-19 00:22:10 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
 
 int main(void)
 {
-  std::cout << "Hello, People! Just a starting point of Tower Defence Experiment." << std::endl;
-  
+  std::cout << "Starting Point - Fullscreen" << std::endl;
+
+  // Get the screen resolution
+  Vector2f resolution;
+	resolution.x = VideoMode::getDesktopMode().width;
+	resolution.y = VideoMode::getDesktopMode().height;
+
+	RenderWindow window(VideoMode(resolution.x, resolution.y),
+		"Tower Defence Experiment", Style::Fullscreen);
+
+  // Create a an SFML View for the main action
+	View mainView(sf::FloatRect(0, 0, resolution.x, resolution.y));
+
+  // run the program as long as the window is open
+  while (window.isOpen())
+  {
+      // check all the window's events that were triggered since the last iteration of the loop
+      sf::Event event;
+      while (window.pollEvent(event))
+      {
+          if(event.type == Event::KeyPressed) {
+              if (event.key.code == Keyboard::Escape){
+                  window.close();
+              }
+          }
+
+      }
+  }
+
   return 0;
 }
