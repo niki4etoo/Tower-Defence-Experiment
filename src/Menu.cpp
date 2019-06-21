da3668393001257a2b47fe44e61864c36cf3d260
Nikolay Nanev <niki4etooo@gmail.com>
2019-06-21 21:45:20 +0300

    Add files via upload

---
+++ src/Menu.cpp
@@ -0,0 +1,18 @@
+#include <SFML/Graphics>
+#include "include/Menu.h"
+
+Menu::loadWaves(){
+  //to do
+}
+
+Menu::loadScores(){
+  //to do
+}
+
+Menu::loadResources(){
+  //to do
+}
+
+Menu::loadRemainingLives(){
+  //to do
+}
