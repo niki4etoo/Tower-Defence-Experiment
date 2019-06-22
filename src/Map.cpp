6dbb684023673702ab682f5fa511da228172a2ba
Nikolay Nanev <niki4etooo@gmail.com>
2019-06-22 23:24:46 +0300

    Add files via upload

---
+++ src/Map.cpp
@@ -0,0 +1,26 @@
+#include <SFML/Graphics.hpp>
+
+// Drawing the main path
+Map::drawPath(float pos_x, float pos_y){
+  //to do
+}
+// Drawing the initial position of the towers
+Map::drawTower(float pos_x, float pos_y){
+  //to do
+}
+// Drawing the initial position of the land
+Map::drawLand(float pos_x, float pos_y){
+  //to do
+}
+// Drawing the grass on the map
+Map::drawGrass(float pos_x, float pos_y){
+  //to do
+}
+// Drawing the scores
+Map::drawScores(float pos_x, float pos_y){
+  //to do
+}
+// Drawing the menu
+Map::drawMenu(float pos_x, float pos_y){
+  //to do
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
+#include "../include/Map.hpp"
+
 using namespace sf;
 
 
@@ -19,6 +21,11 @@ int main(void)
   // Create a an SFML View for the main action
 	View mainView(sf::FloatRect(0, 0, resolution.x, resolution.y));
 
+  //Drawing the main elements in the game ( to do )
+  RectangleShape exampleMenu(Vector2f(resolution.x / 2, 60));
+  exampleMenu.setPosition(resolution.x / 2, 0);
+  exampleMenu.setFillColor(Color::White);
+
   // run the program as long as the window is open
   while (window.isOpen())
   {
@@ -33,6 +40,11 @@ int main(void)
           }
 
       }
+      window.clear();
+
+      window.draw(exampleMenu);
+
+      window.display();
   }
 
   return 0;
