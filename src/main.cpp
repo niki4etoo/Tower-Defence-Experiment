a5fcdf1bf9a613b7b893f0693ae94210bff5acfc
Nikolay Nanev <niki4etooo@gmail.com>
2019-06-24 00:41:08 +0300

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -22,9 +22,25 @@ int main(void)
 	View mainView(sf::FloatRect(0, 0, resolution.x, resolution.y));
 
   //Drawing the main elements in the game ( to do )
+  // ExampleMenu
   RectangleShape exampleMenu(Vector2f(resolution.x / 2, 60));
   exampleMenu.setPosition(resolution.x / 2, 0);
   exampleMenu.setFillColor(Color::White);
+
+  //Drawing the path
+  RectangleShape beginPath;
+  beginPath.setSize(Vector2f(resolution.x - (resolution.x / 2), 100));
+  beginPath.setPosition(Vector2f(resolution.x - (resolution.x / 2), 100));
+  beginPath.setFillColor(Color(150, 150, 180));
+
+  RectangleShape middlePath;
+  middlePath.setSize(Vector2f(100, 400));
+  middlePath.setPosition(Vector2f(resolution.x - (resolution.x / 2), 100));
+  middlePath.setFillColor(Color(150, 150, 180));
+  RectangleShape endPath;
+  endPath.setSize(Vector2f(500, 100));
+  endPath.setPosition(Vector2f(resolution.x - (resolution.x / 2) - 500, 400));
+  endPath.setFillColor(Color(150, 150, 180));
 
   // run the program as long as the window is open
   while (window.isOpen())
@@ -42,7 +58,13 @@ int main(void)
       }
       window.clear();
 
+      //Drawing the example menu on the map
       window.draw(exampleMenu);
+
+      //Drawing the path on the map
+      window.draw(beginPath);
+      window.draw(middlePath);
+      window.draw(endPath);
 
       window.display();
   }
