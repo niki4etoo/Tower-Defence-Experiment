cc77fb3264cf3820948b81f7aadd114946ce07e9
Nikolay Nanev <niki4etooo@gmail.com>
2019-06-25 20:45:39 +0300

    Add files via upload

--- include/Map.hpp
+++ include/Map.hpp
@@ -2,9 +2,17 @@
 
 class Map {
 private:
+
   // Position of every element on the map
-  float tower_pos_x;
-  float tower_pos_y;
+  float tower1_init_out_pos_x;
+  float tower1_init_out_pos_y;
+  float tower2_init_out_pos_x;
+  float tower2_init_out_pos_y;
+
+  float tower1_init_in_pos_x;
+  float tower1_init_in_pos_y;
+  float tower2_init_in_pos_x;
+  float tower2_init_in_pos_y;
 
   //Path
   float path_pos_x;
@@ -26,8 +34,28 @@ private:
   float grass_pos_x;
   float grass_pos_y;
 
-
 public:
+  //Getters and Setters
+  float get_tower1_init_in_pos_x();
+  float get_tower2_init_in_pos_x();
+  float get_tower1_init_in_pos_y();
+  float get_tower2_init_in_pos_y();
+
+  void set_tower1_init_in_pos_x(float x);
+  void set_tower2_init_in_pos_x(float x);
+  void set_tower1_init_in_pos_y(float y);
+  void set_tower2_init_in_pos_y(float y);
+
+  float get_tower1_init_out_pos_x();
+  float get_tower2_init_out_pos_x();
+  float get_tower1_init_out_pos_y();
+  float get_tower2_init_out_pos_y();
+
+  void set_tower1_init_out_pos_x(float x);
+  void set_tower2_init_out_pos_x(float x);
+  void set_tower1_init_out_pos_y(float y);
+  void set_tower2_init_out_pos_y(float y);
+
   //Drawing The Elements on the map
   // Drawing the main path
   void drawPath(float pos_x, float pos_y);
