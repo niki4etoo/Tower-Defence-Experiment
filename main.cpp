5918e8ee3bb34aa333562ecce062b10a5b9b1ee5
Nikolai Nanev <niki4etooo@gmail.com>
2020-11-07 07:36:44 +0200

    Removed mouse button pressed

--- main.cpp
+++ main.cpp
@@ -17,8 +17,7 @@ struct MyApp : App {
         cout << x << " " << y << endl;
     }
     virtual void onMouseDown(int button, int action){
-        if (action==GLFW_PRESS) cout << "mouse pressed" <<endl;
-        if (action==GLFW_RELEASE) cout << "mouse released" <<endl;
+        
     }
     virtual void onKeyDown(int key, int action){
         cout << (unsigned char)key << endl;
