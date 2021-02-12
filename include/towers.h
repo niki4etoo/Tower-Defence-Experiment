a1efcad26e1d8efe340aa8862c14d6c4a4e92aa2
Nikolai Nanev <niki4etooo@gmail.com>
2021-02-12 08:13:52 +0200

    Adding towers properties, setters and getters

--- include/towers.h
+++ include/towers.h
@@ -8,10 +8,19 @@
 #ifndef TOWERS_H_
 #define TOWERS_H_
 
+#include <cstddef>
+
+#define MAX_COUNT_TOWERS 20
+
 class Towers {
+	size_t number_of_towers;
+	int towers_positions[2][MAX_COUNT_TOWERS]; // x, y coordination
 public:
 	Towers();
-	virtual ~Towers();
+	~Towers();
+	void setTowersCount(size_t number_of_towers);
+	size_t getTowersCount();
+	void setTowersPositions(int towers_positions[2][MAX_COUNT_TOWERS]);
 };
 
 #endif /* TOWERS_H_ */
--- src/towers/towers.cpp
+++ src/towers/towers.cpp
@@ -5,13 +5,27 @@
  *      Author: dexter
  */
 
-#include "../include/Towers.h"
+#include "../../include/towers.h"
 
 Towers::Towers() {
 
 }
 
-Towers::~Towers() {
+Towers::~Towers() {}
 
+void Towers::setTowersCount(size_t number_of_towers){
+	this->number_of_towers = number_of_towers;
 }
 
+size_t Towers::getTowersCount(){
+	return this->number_of_towers;
+}
+
+void Towers::setTowersPositions(int towers_positions[2][MAX_COUNT_TOWERS]){
+	//Fill with zeros
+	for(int i = 0; i < MAX_COUNT_TOWERS; i++)
+	{
+		this->towers_positions[0][i] = 0;
+		this->towers_positions[1][i] = 0;
+	}	
+}
