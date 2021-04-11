ecda6d5aff687fef41c07bb045f7d60528aa945a
Nikolai Nanev <niki4etooo@gmail.com>
2021-04-11 10:28:21 +0300

    added inventory tower properties

--- include/ui/inventory.h
+++ include/ui/inventory.h
@@ -2,10 +2,21 @@
 #define INVENTORY_H_
 
 #include <cstddef>
+#include <string>
 
 class Inventory {
+	//Inventory parameters
 	size_t id;
 	size_t numberOfItems;
+	
+	//Towers
+	size_t numberOfTowers;
+	
+	size_t towerType;
+	std::string towerName;
+	size_t towerDamage;
+	size_t towerRange;
+	size_t towerShield;
 	// to do
 public:
 	Inventory();
@@ -15,9 +26,25 @@ public:
 	void setID(size_t id);
 	void setNumberOfItems(size_t numberOfItems);
 	
+	void setNumberOfTowers(size_t numberOfTowers);
+	
+	void setTowerType(size_t towerType);
+	void setTowerName(std::string towerName);
+	void setTowerDamage(size_t towerDamage);
+	void setTowerRange(size_t towerRange);
+	void setTowerShield(size_t towerShield);
+	
 	//Getters
 	size_t getID();
 	size_t getNumberOfItems();
+	
+	size_t getNumberOfTowers();
+	
+	size_t getTowerType();
+	std::string getTowerName();
+	size_t getTowerDamage();
+	size_t getTowerRange();
+	size_t getTowerShield();
 	
 };
 
--- src/ui/inventory.cpp
+++ src/ui/inventory.cpp
@@ -13,6 +13,30 @@ void Inventory::setNumberOfItems(size_t numberOfItems){
 	this->numberOfItems = numberOfItems;
 }
 
+void Inventory::setNumberOfTowers(size_t numberOfTowers){
+	this->numberOfTowers = numberOfTowers;
+}
+	
+void Inventory::setTowerType(size_t towerType){
+	this->towerType = towerType;
+}
+
+void Inventory::setTowerName(std::string towerName){
+	this->towerName = towerName;
+}
+
+void Inventory::setTowerDamage(size_t towerDamage){
+	this->towerDamage = towerDamage;
+}
+
+void Inventory::setTowerRange(size_t towerRange){
+	this->towerRange = towerRange;
+}
+
+void Inventory::setTowerShield(size_t towerShield){
+	this->towerShield = towerShield;
+}
+
 //Getters
 size_t Inventory::getID(){
 	return this->id;
@@ -20,4 +44,28 @@ size_t Inventory::getID(){
 
 size_t Inventory::getNumberOfItems(){
 	return this->numberOfItems;
+}
+
+size_t Inventory::getNumberOfTowers(){
+	return this->numberOfTowers;
+}
+
+size_t Inventory::getTowerType(){
+	return this->towerType;
+}
+
+std::string Inventory::getTowerName(){
+	return this->towerName;
+}
+
+size_t Inventory::getTowerDamage(){
+	return this->towerDamage;
+}
+
+size_t Inventory::getTowerRange(){
+	return this->towerRange;
+}
+
+size_t Inventory::getTowerShield(){
+	return this->towerShield;
 }
