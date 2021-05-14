1f539c0dff4c9473644364131f7db4516643ca69
Nikolai Nanev <niki4etooo@gmail.com>
2021-05-14 21:18:05 +0300

    added hud status bars(player, wave and points)

--- include/player/hud.h
+++ include/player/hud.h
@@ -9,6 +9,20 @@ class HUD {
 	size_t id;
 	std::string name;
 	std::string mainStatusBar;
+	
+	//Wave status
+	size_t waveCount;
+	size_t waveID; //To Do (allocate memory for number of waves)
+	std::string waveName;
+	std::string waveType;
+	
+	//Player status
+	size_t playerID;
+	size_t healthBarPlayer;
+	
+	//Points status
+	size_t points;
+	
 public:
 	HUD(){};
 	~HUD(){};
@@ -19,10 +33,36 @@ public:
 	void setName(std::string name);
 	void setMainStatusBar(std::string mainStatusBar);
 	
+	//Wave status
+	void setWaveCount(size_t waveCount);
+	void setWaveID(size_t waveID); //To Do (allocate memory for number of waves)
+	void setWaveName(std::string waveName);
+	void setWaveType(std::string waveType);
+	
+	//Player status
+	void setPlayerID(size_t playerID);
+	void setHealthBarPlayer(size_t healthBarPlayer);
+	
+	//Points status
+	void setPoints(size_t points);
+	
 	//Getters
 	size_t getID();
 	std::string getName();
 	std::string getMainStatusBar();
+	
+	//Wave status
+	size_t getWaveCount();
+	size_t getWaveID(); //To Do (allocate memory for number of waves)
+	std::string getWaveName();
+	std::string getWaveType();
+	
+	//Player status
+	size_t getPlayerID();
+	size_t getHealthBarPlayer();
+	
+	//Points status
+	size_t getPoints();
 };
 
 #endif
--- src/player/hud.cpp
+++ src/player/hud.cpp
@@ -13,6 +13,36 @@ void HUD::setMainStatusBar(std::string mainStatusBar){
 	this->mainStatusBar = mainStatusBar;
 }
 
+//Wave status
+void HUD::setWaveCount(size_t waveCount){
+	this->waveCount = waveCount;
+}
+
+void HUD::setWaveID(size_t waveID){ //To Do (allocate memory for number of waves)
+	this->waveID = waveID;
+}
+
+void HUD::setWaveName(std::string waveName){
+	this->waveName = waveName;
+}
+
+void HUD::setWaveType(std::string waveType){
+	this->waveType = waveType;
+}
+
+//Player status
+void HUD::setPlayerID(size_t playerID){
+	this->playerID = playerID;
+}
+
+void HUD::setHealthBarPlayer(size_t healthBarPlayer){
+	this->healthBarPlayer = healthBarPlayer;
+}
+
+//Points status
+void HUD::setPoints(size_t points){
+	this->points = points;
+}
 
 //Getters
 size_t HUD::getID(){
@@ -25,4 +55,37 @@ std::string HUD::getName(){
 
 std::string HUD::getMainStatusBar(){
 	return this->mainStatusBar;
+}
+
+//Wave status
+size_t HUD::getWaveCount(){
+	return this->waveCount;
+}
+
+size_t HUD::getWaveID(){ //To Do (allocate memory for number of waves)
+	return this->waveID;
+}
+
+std::string HUD::getWaveName(){
+	return this->waveName;
+}
+
+std::string HUD::getWaveType(){
+	return this->waveType;
+}
+
+
+//Player status
+size_t HUD::getPlayerID(){
+	return this->playerID;
+}
+
+size_t HUD::getHealthBarPlayer(){
+	return this->healthBarPlayer;
+}
+
+
+//Points status
+size_t HUD::getPoints(){
+	return this->points;
 }
