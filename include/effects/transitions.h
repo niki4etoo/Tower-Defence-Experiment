f144749363a5fb31c80caf48b7f7e6339e8ed496
Nikolai Nanev <niki4etooo@gmail.com>
2021-05-20 22:07:35 +0300

    added setters and getters for transitions class

--- include/effects/transitions.h
+++ include/effects/transitions.h
@@ -1,13 +1,28 @@
 #ifndef TRANSITIONS_H_
 #define TRANSITIONS_H_
 
-class Transitions {
+#include <cstddef>
+#include <string>
 
+class Transitions {
+	size_t id;
+	std::string type;
+	std::string effectiveness;
 public:
 	Transitions(){};
 	~Transitions(){};
 	
 	void slide(); //To do (Slide effects for game transitions of states)
+
+	//Setters
+	void setID(size_t id);
+	void setType(std::string type);
+	void setEffectiveness(std::string effectiveness);
+	
+	//Getters
+	size_t getID();
+	std::string getType();
+	std::string getEffectiveness();
 
 };
 
--- src/effects/transitions.cpp
+++ src/effects/transitions.cpp
@@ -5,3 +5,29 @@ void Transitions::slide(){
 	//To do
 
 }
+
+//Setters
+void Transitions::setID(size_t id){
+	this->id = id;
+}
+
+void Transitions::setType(std::string type){
+	this->type = type;
+}
+
+void Transitions::setEffectiveness(std::string effectiveness){
+	this->effectiveness = effectiveness;
+}
+
+//Getters
+size_t Transitions::getID(){
+	return this->id;
+}
+
+std::string Transitions::getType(){
+	return this->type;
+}
+
+std::string Transitions::getEffectiveness(){
+	return this->effectiveness;
+}
